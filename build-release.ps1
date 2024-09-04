1d0d8d099398b4821f7f7cae45500ba653d3b970
yoelboim <yboim@tenable.com>
2024-09-04 17:37:50 +0300

    publish release

--- build-release.ps1
+++ build-release.ps1
@@ -1,4 +1,5 @@
 ﻿helm package .\charts\cloud-security\endpoint-connector -d .\releases
+helm package .\charts\cloud-security\kubernetes-cluster -d .\releases
 helm package .\charts\cloud-security\kubernetes-cluster-connector -d .\releases
 helm package .\charts\security-center -d .\releases
 
