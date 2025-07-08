c6a2e015e1cfb3a07d9c9bd1ccb4bcf388658aec
Alex Plits <aplits@tenable.com>
2025-07-08 14:35:50 +0300

    remove old helm releases

--- build-release.sh
+++ build-release.sh
@@ -1,6 +1,5 @@
 helm package ./charts/cloud-security/endpoint-connector -d ./releases
 helm package ./charts/cloud-security/kubernetes-cluster -d ./releases
-helm package ./charts/cloud-security/kubernetes-cluster-connector -d ./releases
 helm package ./charts/security-center -d ./releases
 
 helm repo index .
